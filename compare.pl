277fc37d76ac65a53d8f4c6a3c13bfef1e2775d0
Scott Baker <scott@perturb.org>
2026-02-06 16:51:10 -0800

    New `color()` function that allows text

--- compare.pl
+++ compare.pl
@@ -152,7 +152,10 @@ sub trim {
 
 # String format: '115', '165_bold', '10_on_140', 'reset', 'on_173', 'red', 'white_on_blue'
 sub color {
-	my $str = shift();
+	my ($str, $txt) = @_;
+
+	# If we're NOT connected to a an interactive terminal don't do color
+	if (-t STDOUT == 0) { return $txt // ""; }
 
 	# No string sent in, so we just reset
 	if (!length($str) || $str eq 'reset') { return "\e[0m"; }
@@ -165,14 +168,17 @@ sub color {
 	my ($fc,$cmd) = $str =~ /^(\d{1,3})?_?(\w+)?$/g;
 	my ($bc)      = $str =~ /on_(\d{1,3})$/g;
 
+	if (defined($fc) && int($fc) > 255) { $fc = undef; } # above 255 is invalid
+
 	# Some predefined commands
 	my %cmd_map = qw(bold 1 italic 3 underline 4 blink 5 inverse 7);
 	my $cmd_num = $cmd_map{$cmd // 0};
 
 	my $ret = '';
-	if ($cmd_num)     { $ret .= "\e[${cmd_num}m"; }
-	if (defined($fc)) { $ret .= "\e[38;5;${fc}m"; }
-	if (defined($bc)) { $ret .= "\e[48;5;${bc}m"; }
+	if ($cmd_num)      { $ret .= "\e[${cmd_num}m"; }
+	if (defined($fc))  { $ret .= "\e[38;5;${fc}m"; }
+	if (defined($bc))  { $ret .= "\e[48;5;${bc}m"; }
+	if (defined($txt)) { $ret .= $txt . "\e[0m";   }
 
 	return $ret;
 }
